6c62c86d7b501ec9a7f8c09b51dec1fe879f9ad0
jormungandr3000 <jormungandr3000@gmail.com>
2021-07-13 02:26:27 +0300

    working 8.0

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@
 #include <map>
 #include <thread>
 
-void counting_words(const std::vector<std::string>& dictionary, std::vector<std::map<std::string, size_t>> all_maps, const size_t limit,const size_t number_of_thread){
+void counting_words(const std::vector<std::string>& dictionary, std::vector<std::map<std::string, size_t>>& all_maps, const size_t limit,const size_t number_of_thread){
     std::string word;
     size_t other_words = 0;
     if (number_of_thread == all_maps.size()-1)
@@ -14,13 +14,13 @@ void counting_words(const std::vector<std::string>& dictionary, std::vector<std:
     for (int i = number_of_thread*limit; i < (number_of_thread+1)*limit+other_words; ++i) {
         word = dictionary[i];
         preprocessing(word);
-        all_maps[number_of_thread][word] += 1;
+        map_word_adder(word, all_maps[number_of_thread]);
         if (i==dictionary.size()-1)
             break;
     }
 }
 
-void merge_all_maps(std::vector<std::map<std::string, size_t>> all_maps){
+void merge_all_maps(std::vector<std::map<std::string, size_t>>& all_maps){
     for (size_t i = 1; i < all_maps.size(); ++i) {
         for (auto const&[word, number] : all_maps[i]) {
             all_maps[0][word] += number;
